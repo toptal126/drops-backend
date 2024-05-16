5e86ec8cf8e02b8a1207f06554a1f9f1ffa0f410
toptal126 <toptal126@gmail.com>
2024-05-16 09:30:58 -0500

    add mail moduler

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,27 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { MailService } from './mail/mail.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly mailService: MailService,
+  ) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('test')
+  testFunc() {
+    return this.mailService.sendUserConfirmation(
+      {
+        email: 'toptal126@gmail.com',
+        name: 'Toptal126',
+      },
+      '1234',
+    );
   }
 }
---
+++ src/mail/mail.service.ts
@@ -0,0 +1,24 @@
+import { MailerService } from '@nestjs-modules/mailer';
+import { Injectable } from '@nestjs/common';
+import { User } from './../user/user.entity';
+
+@Injectable()
+export class MailService {
+  constructor(private mailerService: MailerService) {}
+
+  async sendUserConfirmation(user: User, token: string) {
+    const url = `example.com/auth/confirm?token=${token}`;
+
+    await this.mailerService.sendMail({
+      to: user.email,
+      subject: 'Welcome to Nice App! Confirm your Email',
+
+      template: './confirmation', // ✅ template found again in v1.6.0
+
+      context: {
+        name: user.name,
+        url,
+      },
+    });
+  }
+}
